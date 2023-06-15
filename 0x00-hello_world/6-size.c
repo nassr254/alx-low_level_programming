61419bbb70b1d1a6a5929f0a8d6b482c9c0d925e
nassr254 <nasrrahim254@gmail.com>
2023-06-15 04:45:51 -0700

    Hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ int main(void)
+ {
+ print("Size of a char: %d byte(s)\n", size of (char));
+  print("Size of an int: %d byte(s)\n", size of (int));
+   print("Size of a lomg int: %d byte(s)\n", size of (long int));
+    print("Size of a long long int: %d byte(s)\n", size of (long long int));
+     print("Size of a float: %d byte(s)\n", size of (float));
+     return(0);
+ }
